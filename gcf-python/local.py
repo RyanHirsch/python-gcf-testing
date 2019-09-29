9eb7d1f9b44f45595d42350afb6e489150722bfc
Ryan Hirsch <ryan.hirsch@mixpanel.com>
2019-09-28 17:55:14 -0700

    fix module resolution when running locally

--- gcf-python/local.py
+++ gcf-python/local.py
@@ -1,5 +1,14 @@
+import sys
+import os
 import importlib
+import site
 from flask import Flask, request
+
+# Manipulate our local module resolution to match that which will
+#  be occuring on GCP
+this_dir = os.path.dirname(os.path.abspath(__file__))
+for sub_dir in next(os.walk(this_dir))[1]:
+    site.addsitedir(f"{this_dir}/{sub_dir}")
 
 app = Flask(__name__)
 
